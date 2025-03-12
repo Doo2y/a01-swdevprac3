324a7ab1414eec69f37344b350f213e35077b8d0
Doo2y <kristana833@gmail.com>
2025-03-12 22:16:38 +0700

    as3

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,20 @@
+import styles from './productcard.module.css'
+import Image from 'next/image';
+
+export default function ProductCard() {
+    return (
+        <div className={styles.card}> 
+            <div className={styles.cardimg}>
+                <Image src={'/img/event.jpeg'}
+                alt='Product Picture'
+                fill={true}
+                objectFit='cover'
+                />
+            </div>
+            <div className = {styles.cardtext} > 
+                <h2>graduation party</h2>
+                <h4> A restaurant decorated with a beautiful atmosphere on the day of the graduation ceremony.</h4> </div>
+
+        </div>
+    );
+}
