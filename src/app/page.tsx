7a99cb93c751006353ab927c75e05c7633f79dd6
Doo2y <kristana833@gmail.com>
2025-03-12 22:25:03 +0700

    edit final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Banner from '@/components/Banner';
-import Productcard from "@/components/ProductCard";
+import Productcard from "@/components/Card";
 
 export default function Home() {
   return (
     <main>
       <Banner/>
-      <div style = {{margin: "20px", display:"flex", 
-        flexDirection:"row", alignContent:"space-around", 
-        justifyContent:"space-around", flexWrap:"wrap"
-      }}>
-        <Productcard/>
-        <Productcard/>
-        <Productcard/>
+      <div style = {{margin: "20px"}}>
         <Productcard/>
       </div>
     </main>
